b4a8ec5b11216dddd41b67243988dc29f4883fc2
Schreiby <ettie.schreiber@gmail.com>
2015-07-09 18:00:25 -0400

    Performing long-running tasks in a service

--- app/src/main/java/com/example/ettie/servicesdemo/MyServices.java
+++ app/src/main/java/com/example/ettie/servicesdemo/MyServices.java
@@ -3,15 +3,20 @@ package com.example.ettie.servicesdemo;
 /**
  * Created by Ettie on 7/9/2015.
  */
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.widget.Toast;
+import android.os.AsyncTask;
+import android.util.Log;
 
 public class MyServices extends Service{
 
     @Override
+
     public IBinder onBind(Intent arg0) {
         //TODO
         return null;
@@ -19,7 +24,15 @@ public class MyServices extends Service{
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        /* initial commit:
         Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        return START_STICKY;*/
+
+        try{
+            new DownloadTask().execute(new URL("http://example.com/downloads/file.pdf"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         return START_STICKY;
     }
 
@@ -29,4 +42,32 @@ public class MyServices extends Service{
         Toast.makeText(this, "Service Stopped", Toast.LENGTH_LONG).show();
     }
 
+    private class DownloadTask extends AsyncTask<URL, Integer, String> {
+
+        @Override
+        protected String doInBackground(URL... params) {
+            for (int i = 0; i <= 10; i++) {
+                try{
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                publishProgress(i * 10);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            Log.d("DownloadTask", values[0] + "% downloaded");
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Toast.makeText(getBaseContext(), "Download Complete", Toast.LENGTH_LONG).show();
+            stopSelf();
+        }
+    }
 }
